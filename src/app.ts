93db2ad3cbc6f12b9162f18ec2c877e90d3c4ca4
Sarker-Rajib <redhat0593@gmail.com>
2024-07-24 15:19:48 +0600

    basic setup done

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+// define app
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// app routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("! ==> Welcome to online Nursery <== !");
+});
+
+// error handlers
+
+// not found route
+
+export default app;
